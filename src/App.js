bcdea9d82e47d138b6596f1368222765f5f78ad2
Khalid <114188229+sunsetofmoon@users.noreply.github.com>
2022-12-13 21:17:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [check, setCheck] = useState(false)
+
+  const [todos, setTodos] = useState([])
+
+  const [text, setText] = useState('')
+
+  function toggleCheck(e){
+    setCheck(e.target.checked)
+  }
+
+  function handlerChange(e){
+    setText(e.target.value)
+  }
+
+  function handlerBtn(){
+    setTodos([
+      {
+        text: text,
+        check: check
+      },
+      ...todos
+    ])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="inputs">
+        <input className="text" type='text' placeholder="Введите сообщение..." value={text} onChange={handlerChange}/>
+        <input className="checkbox" type='checkbox' checked={check} onChange={toggleCheck}/>
+        <button className="btn" onClick={handlerBtn}>→</button>
+      </div>
+
+      {todos.map(item =>{
+        return(
+        <li>
+          <div className="test">
+          {item.check ?  <>
+            {item.text}
+            <input className="checkboxTodo" type='checkbox' checked={item.check} />
+          </> : item.text}
+          </div>
+        </li>
+        )
+      })}
     </div>
   );
 }
